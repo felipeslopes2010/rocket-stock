52f4e7db4dba1b83403d7436f38e867bedba9abc
Felipe Santos <felipe.santos@qualyteam.com.br>
2024-10-02 15:11:21 -0300

    Adjust: Separating routes by roles

--- web/src/routes/index.jsx
+++ web/src/routes/index.jsx
@@ -1,16 +1,32 @@
 import { BrowserRouter } from 'react-router-dom';
 
 import { useAuth } from "../hooks/auth";
+import { USER_ROLE } from "../utils/roles"
 
 import { AdminRoutes } from './admin.routes';
+import { SalesRoutes } from './sales.routes';
+import { CustomerRoutes } from './customer.routes';
 import { AuthRoutes } from './auth.routes';
 
 export function Routes() {
   const { user } = useAuth();
 
+  function AccessRoute() {
+    switch(user.role) {
+      case USER_ROLE.ADMIN:
+        return <AdminRoutes />;
+      case USER_ROLE.SALES:
+        return <SalesRoutes />;
+      case USER_ROLE.CUSTOMER:
+        return <CustomerRoutes />;
+      default:
+        return <CustomerRoutes />;
+    }
+  }  
+
   return (
     <BrowserRouter>
-      {user ? <AdminRoutes /> : <AuthRoutes />}
+      {user ? <AccessRoute />  : <AuthRoutes />}
     </BrowserRouter>
   );
 }
--- web/src/routes/sales.routes.jsx
+++ web/src/routes/sales.routes.jsx
@@ -5,7 +5,7 @@ import { Product } from '../pages/Product';
 import { NotFound } from '../pages/NotFound';
 import { SalesReport } from '../pages/SalesReport';
 
-export function AdminRoutes() {
+export function SalesRoutes() {
   return (
     <Routes>
       <Route path="/" element={< Home />} />
