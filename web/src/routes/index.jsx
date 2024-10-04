ac8bdc3f6dea23b77f0850e9374a39e583d06902
Felipe Santos <felipe.santos@qualyteam.com.br>
2024-10-04 17:05:43 -0300

    Adjust: Change local storage token to Cookie

--- web/src/routes/index.jsx
+++ web/src/routes/index.jsx
@@ -1,6 +1,8 @@
+import { useEffect } from "react";
 import { BrowserRouter } from 'react-router-dom';
 
 import { useAuth } from "../hooks/auth";
+import { api } from "../services/api"
 import { USER_ROLE } from "../utils/roles"
 
 import { AdminRoutes } from './admin.routes';
@@ -8,8 +10,18 @@ import { SalesRoutes } from './sales.routes';
 import { CustomerRoutes } from './customer.routes';
 import { AuthRoutes } from './auth.routes';
 
+
 export function Routes() {
-  const { user } = useAuth();
+  const { user, signOut } = useAuth();
+
+  useEffect(() => {
+    api.get('/users/validated')
+    .catch((error) => {
+     if(error.response?.status === 401) {
+       signOut();
+     }
+    })
+  }, []);
 
   function AccessRoute() {
     switch(user.role) {
